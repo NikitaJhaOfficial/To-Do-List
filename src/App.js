e52b6bd867d3aad58eac35da9b18a493da3ae65a
NIKITA JHA <86097451+NIKITA-12CRAZY@users.noreply.github.com>
2023-02-26 12:12:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todolist from './Todolist';
+//TODO LIST
+function App() {
+  return (
+    <div className="App">
+      <Todolist/>
+      
+    </div>
+  );
+}
+
+export default App;
